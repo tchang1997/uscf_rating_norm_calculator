13c6bdafb552538fc4eb35e2714a0594d9da06c0
Trenton Chang <tchang97@cs.stanford.edu>
2022-10-02 15:01:47 -0400

    new shiny readme

--- README.md
+++ README.md
@@ -1,33 +1,36 @@
 # USCF Norm and Rating Calculator
 
-A command-line utility that lets you calculate USCF rating information and norms. Can compute expected results, expected wins, and other info. 
+A command-line utility that lets you calculate USCF rating information and norms. Can compute expected results, expected wins, and other info. This isn't thoroughly tested or affiliated with the USCF in any way -- just another informal tool.
 
 Example usage:
 ```
-python main.py --curr-rating 1470 --opp-ratings 974 951 2471 1185 --score 3 --prev-games 25
+python main.py --curr-rating 1470 --opp-ratings 974 951 2471 1585 --score 3 --prev-games 25
 ```
 Example output:
 
+
 ```
 Rating update: 1470.0000 -> 1505.4203 (+35.4203)
+Performance: 2033.3468
 Performance above/below expected: +0.7590 (Expected: 2.2410)
-Performance: 2033.3467992502594
-Norm: 4th
-┏━━━━━━━━━━━━━━━━┳━━━━━━━━┳━━━━━━━━┳━━━━━━━━┳━━━━━━━━┳━━━━━━━━┳━━━━━━━━┳━━━━━━━━┓
-┃ Round #        ┃ 4th    ┃ 3rd    ┃ 2nd    ┃ 1st    ┃ C      ┃ M      ┃ S      ┃
-┡━━━━━━━━━━━━━━━━╇━━━━━━━━╇━━━━━━━━╇━━━━━━━━╇━━━━━━━━╇━━━━━━━━╇━━━━━━━━╇━━━━━━━━┩
-│ 1              │ 0.786  │ 0.9207 │ 0.9735 │ 0.9915 │ 0.9973 │ 0.9991 │ 0.9997 │
-│ 2              │ 0.8074 │ 0.9299 │ 0.9767 │ 0.9925 │ 0.9976 │ 0.9992 │ 0.9998 │
-│ 3              │ 0.0007 │ 0.0021 │ 0.0066 │ 0.0206 │ 0.0623 │ 0.1736 │ 0.3992 │
-│ 4              │ 0.0983 │ 0.2564 │ 0.5216 │ 0.7752 │ 0.916  │ 0.9718 │ 0.9909 │
-│ Expected Score │ 1.6924 │ 2.1091 │ 2.4784 │ 2.7797 │ 2.9732 │ 3.1438 │ 3.3896 │
-│ Required Score │ 2.6924 │ 3.1091 │ 3.4784 │ 3.7797 │ 3.9732 │ N/A    │ N/A    │
-│ Norm Earned    │ YES    │ NO     │ NO     │ NO     │ NO     │ NO     │ NO     │
-└────────────────┴────────┴────────┴────────┴────────┴────────┴────────┴────────┘
+Norm: 4th (1200)
+                                  Expected and required results/scores by norm level                                  
+┏━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━┓
+┃ Round #        ┃ Opp. Ratings ┃ 4th (1200) ┃ 3rd (1400) ┃ 2nd (1600) ┃ 1st (1800) ┃ C (2000) ┃ M (2200) ┃ S (2400) ┃
+┡━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━━┩
+│ 1              │ 974.0        │ 0.786      │ 0.9207     │ 0.9735     │ 0.9915     │ 0.9973   │ 0.9991   │ 0.9997   │
+│ 2              │ 951.0        │ 0.8074     │ 0.9299     │ 0.9767     │ 0.9925     │ 0.9976   │ 0.9992   │ 0.9998   │
+│ 3              │ 2471.0       │ 0.0007     │ 0.0021     │ 0.0066     │ 0.0206     │ 0.0623   │ 0.1736   │ 0.3992   │
+│ 4              │ 1585.0       │ 0.0983     │ 0.2564     │ 0.5216     │ 0.7752     │ 0.916    │ 0.9718   │ 0.9909   │
+├────────────────┼──────────────┼────────────┼────────────┼────────────┼────────────┼──────────┼──────────┼──────────┤
+│ Expected Score │ N/A          │ 1.6924     │ 2.1091     │ 2.4784     │ 2.7797     │ 2.9732   │ 3.1438   │ 3.3896   │
+│ Required Score │ N/A          │ 2.6924     │ 3.1091     │ 3.4784     │ 3.7797     │ 3.9732   │ N/A      │ N/A      │
+├────────────────┼──────────────┼────────────┼────────────┼────────────┼────────────┼──────────┼──────────┼──────────┤
+│ Norm Earned    │ N/A          │ YES        │ NO         │ NO         │ NO         │ NO       │ NO       │ NO       │
+└────────────────┴──────────────┴────────────┴────────────┴────────────┴────────────┴──────────┴──────────┴──────────┘
 ```
+
 
 ## Limitations
 
 May not work with provisional ratings as well, since a special rating formula is used for computing those.  
-
-
--- main.py
+++ main.py
@@ -11,18 +11,18 @@ from rich.table import Table
 USCF_FLOOR = 100
 USCF_BONUS_MULTIPLIER = 14
 NORMS = {
-    1200: "4th",
-    1400: "3rd",
-    1600: "2nd",
-    1800: "1st",
-    2000: "C",
-    2200: "M",
-    2400: "S",
+    1200: "4th (1200)",
+    1400: "3rd (1400)",
+    1600: "2nd (1600)",
+    1800: "1st (1800)",
+    2000: "C (2000)",
+    2200: "M (2200)",
+    2400: "S (2400)",
 }
 
 def get_args():
     psr = ArgumentParser()
-    psr.add_argument("--curr-rating", type=float, required=True, help="Your current rating.") # TODO: handle provisional
+    psr.add_argument("--curr-rating", type=float, required=True, help="Your current rating. For unrated players, set this to zero.") # TODO: handle provisional
     psr.add_argument("--prev-games", type=int, help="# of games previously played.", default=25)
     psr.add_argument("--opp-ratings", type=float, required=True, nargs='+', help="List of opponeng ratings.")
     psr.add_argument("--score", type=float, required=True, help="Your tournament score. Must be a multiple of 0.5.")
@@ -33,7 +33,7 @@ def validate(args):
         raise ValueError("Score must be non-negative.")
     if args.score * 2 != int(args.score * 2):
         raise ValueError("Score must be a multiple of 0.5 (loss=0, draw=0.5, win=1).")
-    if args.curr_rating < USCF_FLOOR:
+    if args.prev_games > 0 and args.curr_rating < USCF_FLOOR:
         raise ValueError("Your rating must be above the USCF floor of 100.")
     if any(map(partial(op.gt, USCF_FLOOR), args.opp_ratings)):
         raise ValueError("All opponent ratings must be above the USCF floor of 100.")
@@ -41,6 +41,12 @@ def validate(args):
         raise ValueError("Must have played a positive number of games.")
     if args.prev_games < 8:
         warnings.warn("Rating calculation may be inaccurate for provisional ratings under 8 games.", RuntimeWarning)
+    if args.curr_rating == 0:
+        if args.prev_games > 0:
+            raise ValueError("Cannot have a null rating with games played.")
+        else:
+            warnings.warn("Unrated players are assumed to have an implicit prior rating of 1300, assuming adult USCF membership and no prior US Chess/FIDE/CFC ratings. If you are not unrated, enter your actual rating in the `--curr-rating` argument.")
+            args.curr_rating = 1300
 
 def calculate_effective_games(rating, prev_games):
     if rating <= 2355:
@@ -59,13 +65,12 @@ def compute_k_numerator(rating):
 def get_expected_wins(rating, opps):
     return 1 / (1 + np.power(10, -(rating - np.array(opps)) / 400))
 
+
 def find_performance_rating(init, opps, score, tol=1e-3):
     """
         Solution to optimization problem
 
         minimize_r 1/2 (score - win_expectancy)^2
-
-        -- note -- doesn't work for perfect/imperfect scores
     """
     if score == 0:
         return min(opps) - 400
@@ -80,6 +85,12 @@ def find_performance_rating(init, opps, score, tol=1e-3):
         diff = 100 / inv_lr * grad
         curr_guess -= diff
     return curr_guess
+
+def compute_special_rating(rating, opps, score, prev_games):
+    pwe = np.clip((rating - opps) / 800 + 0.5, 0, 1)
+    effective_games = calculate_effective_games(rating, prev_games)
+    k = compute_k_numerator(rating) / (effective_games + len(opps))
+    #  TODO: finish this
 
 
 def compute_new_rating(rating, opps, score, prev_games):
@@ -116,36 +127,44 @@ def get_norms(rating, opps, score):
     info_df = pd.concat([expected_scores, required_scores, earned], axis=1).T
     norm_df = pd.concat([norm_df, info_df], axis=0)
     norm_df.index.rename('Round #', inplace=True)
+    norm_df = norm_df.reindex(columns=["Opp. Ratings"] + list(norm_df.columns))
+    norm_df.loc[:len(opps), "Opp. Ratings"] = opps
+    norm_df["Opp. Ratings"] = norm_df["Opp. Ratings"].fillna("N/A")
     return norm_df
 
-def df_to_table(df):
+def df_to_table(df, n_games):
     """
         Adapted from: https://gist.github.com/neelabalan/33ab34cf65b43e305c3f12ec6db05938
     """
-    rich_table = Table()
-    rich_table.add_column(df.index.name)
+    rich_table = Table(
+            title="Expected and required results/scores by norm level",
+            header_style="bold cyan",
+            row_styles=[""] * n_games + ["cyan", "cyan", "magenta"],
+    )
+    rich_table.add_column(df.index.name, style="bold", no_wrap=True)
 
     for column in df.columns:
         rich_table.add_column(str(column))
 
-    for index, value_list in zip(df.index, df.values.tolist()):
+    for i, (index, value_list) in enumerate(zip(df.index, df.values.tolist())):
         row = [str(index)]
         row += [str(x) for x in value_list]
-        rich_table.add_row(*row)
+        rich_table.add_row(*row, end_section=(i + 1 == n_games) or (i + 1 == n_games + 2))
 
     return rich_table
 
-def print_results(performance, score, we, curr_rating, new_rating, norms_table):
+def print_results(performance, score, we, curr_rating, new_rating, norms_table, opps):
     rating_delta = new_rating - curr_rating
     sign_color = "red" if rating_delta < 0 else "green"
     print("[bold]Rating update:[/bold]", f"{curr_rating:.4f} -> {new_rating:.4f}", f"[bold {sign_color}]({new_rating - curr_rating:+.4f})[/bold {sign_color}]")
+
+    print("[bold]Performance:[/bold]", f"{performance:.4f}")
     print("[bold]Performance above/below expected:[/bold]", f"[bold {sign_color}]{score - we:+.4f}[/bold {sign_color}] (Expected: {we:.4f})")
-    print("[bold]Performance:[/bold]", performance)
     earned = norms_table.loc["Norm Earned"]
     norm_idx = earned.where(earned=="YES").last_valid_index()
     print("Norm:", norm_idx)
     norms_table = norms_table.applymap(lambda x: np.round(x, 4) if isinstance(x, (int, float)) else x)
-    print(df_to_table(norms_table))
+    print(df_to_table(norms_table, len(opps)))
 
 if __name__ == '__main__':
     args = get_args()
@@ -154,4 +173,4 @@ if __name__ == '__main__':
     we = get_expected_wins(args.curr_rating, args.opp_ratings).sum()
     tpr = find_performance_rating(new_rating, args.opp_ratings, args.score)
     norms_table = get_norms(args.curr_rating, args.opp_ratings, args.score)
-    print_results(tpr, args.score, we, args.curr_rating, new_rating, norms_table)
+    print_results(tpr, args.score, we, args.curr_rating, new_rating, norms_table, args.opp_ratings)
